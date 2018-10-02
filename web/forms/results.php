bbbb5af978081dc6f2a9cbbfb4c73e9f3c2e023f
Scott Burton <burtons@byui.edu>
2018-10-02 09:53:19 -0600

    forms demo

---
+++ web/forms/results.php
@@ -0,0 +1,16 @@
+<?php
+
+$username = $_POST['username'];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h1>Welcome user: <?php echo $username; ?></h1>
+
+</body>
+</html>
