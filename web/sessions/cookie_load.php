56b7776190ce7c6f32b719556f4acd16c10c2976
Scott Burton <burtons@byui.edu>
2018-10-04 16:03:41 -0600

    load cookie

---
+++ web/sessions/cookie_load.php
@@ -0,0 +1,19 @@
+<?php
+
+$favorite = $_COOKIE["fav-text"];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+echo "Your favorite text was $favorite";
+
+?>
+
+</body>
+</html>
