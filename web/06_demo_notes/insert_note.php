abf296f000b5a993fe0ecc251f1e9d90ae21a6ec
Scott Burton <burtons@byui.edu>
2018-10-23 12:35:33 -0600

    fixed url param

--- web/06_demo_notes/insert_note.php
+++ web/06_demo_notes/insert_note.php
@@ -11,7 +11,7 @@ $stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
 $stmt->bindValue(':content', $content, PDO::PARAM_STR);
 $stmt->execute();
 
-$new_page = "course_notes.php?id=$course_id";
+$new_page = "course_notes.php?course_id=$course_id";
 
 header("Location: $new_page");
 die();
