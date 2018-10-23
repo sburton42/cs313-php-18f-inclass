95cf8b61eba5ff705fcdb646b719901bc57504c9
Scott Burton <burtons@byui.edu>
2018-10-23 12:34:06 -0600

    inserting note

--- web/06_demo_notes/insert_note.php
+++ web/06_demo_notes/insert_note.php
@@ -3,7 +3,17 @@
 $course_id = htmlspecialchars($_POST['course_id']);
 $content = htmlspecialchars($_POST['note_content']);
 
-echo "$course_id\n";
-echo $content;
+require('dbConnect.php');
+$db = get_db();
+
+$stmt = $db->prepare('INSERT INTO note(course_id, content) VALUES (:course_id, :content);');
+$stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
+$stmt->bindValue(':content', $content, PDO::PARAM_STR);
+$stmt->execute();
+
+$new_page = "course_notes.php?id=$course_id";
+
+header("Location: $new_page");
+die();
 
 ?>
